1c8068599092db5d248f624d554cea229bd7eea7
Federico Dente <dentefederico@live.com>
2017-04-05 14:31:21 +0200

    added offline function and some basic style

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-var streamers = ["ESL_SC2", "freecodecamp", "xPeke", "rivalxfactor"];
+var streamers = ["ESL_SC2", "freecodecamp", "xPeke", "rivalxfactor", "boxbox", "Scarra", "IWillDominate"];
 
 $.each(streamers, function(i, val) {
 	var url = "https://wind-bow.gomix.me/twitch-api/streams/" + val + "?callback=?";
@@ -11,7 +11,7 @@ $.each(streamers, function(i, val) {
 			online(data);
 		} else if (data.stream === null ){
 			// if the data is null (streaming offline), call the offline function, that will display data  (all the offline channels)
-			offline(data);
+			getUserData(val);
 		}
 		console.log(data);
 
@@ -20,8 +20,11 @@ $.each(streamers, function(i, val) {
 
 
 
+
+
+// if the streamers are online i'll get the data and display it
 function online(data) {
-	// getting data from the API
+	 // getting data from the API
 	var name = data.stream.channel.display_name;
 	var game = data.stream.channel.game;
 	var logo = data.stream.channel.logo;
@@ -29,18 +32,52 @@ function online(data) {
 	var url = data.stream.channel.url;
 	var description = data.stream.channel.status;
 
-	// displaying data on the website
-	$('#img-online').html('<img src=\"' + logo + '\" class=\"stream-logo\">');
-	$('#content').html('<a href=\"' + url + '\" target=\"_blank\" class=\"link\">\"'+ name +'\"</a>');
-	$('#content').append('<p class=\"title\">\"' + status + '\" </p>');
-	$('#content').append('<p class=\"game\"> Game : \"' + game + '\" </p>');
+	 
+	var displayData = '<div class="row ' + 
+          status + '"><div class="col-xs-2 col-md-1"><img src="' + 
+          logo + '" class="img-responsive"></div><div class="col-xs-3 col-md-3" id="name"><a href="' + 
+          url + '" target="_blank">' + 
+          name + '</a></div><div class="col-xs-7 col-md-8">'+ 
+          description + '</div></div>';
+
+         $('.display').append(displayData);
+
+}
+
+// if it's offline, i have to call the api from a different url ("https://wind-bow.gomix.me/twitch-api/channels/") instead of ("https://wind-bow.gomix.me/twitch-api/streams/")
+function getUserData(val) {
+	var channelUrl = "https://wind-bow.gomix.me/twitch-api/channels/" + val + "?callback=?";
+	$.getJSON(channelUrl, function(data) {
+		offline(data);
+	});
 }
 
 
 
-
+// if streamers are offline i'll get the data and display it
 function offline(data) {
-	$('.offline').html('The stream is offline');
+	// getting data from the API
+	var status = data.status;
+	var name = data.display_name;
+	var logo = data.logo;
+	var url = data.url;
+	var game = "Offline";
+	var description = data.description;
+
+
+	var displayOfflineData = '<div class="row ' + 
+          status + '"><div class="col-xs-2 col-md-1"><img src="' + 
+          logo + '" class="img-responsive"></div><div class="col-xs-7 col-md-3" id="name"><a href="' + 
+          url + '" target="_blank">' + 
+          name + '</a></div><div class="col-xs-3 col-md-8">'+ 
+          game + '</div></div>';
+
+
+     $('.offline-display').append(displayOfflineData);
+
+
+
+	console.log(data);
 }
 
 
