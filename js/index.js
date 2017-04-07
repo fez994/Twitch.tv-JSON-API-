bb86da61b375adfa8fc1f089c03f11def1a2ecf7
Federico Dente <dentefederico@live.com>
2017-04-08 01:04:31 +0200

    Added the last user story: i will see a placeholder notificaion if a streamer has closed their twitch account. I don't really like this idea, so i comment out the code.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,20 @@
 $(document).ready(function() {
 
-var streamers = ["ESL_SC2", "freecodecamp", "xPeke", "Doublelift", "boxbox", "Scarra", "IWillDominate", "Gosu",];
+var streamers = ["ESL_SC2", "freecodecamp", "xPeke", "Doublelift", "boxbox", "Scarra", "IWillDominate", "Gosu"];
 
-$.each(streamers, function(i, val) {
+$.each(streamers, function(i, val, und) {
 	var url = "https://wind-bow.gomix.me/twitch-api/streams/" + val + "?callback=?";
 
-	$.getJSON(url, function(data){
+	var get = $.getJSON(url, function(data){
 		if(data.stream !== null) {
 			// if the data is not null (streaming online) , call the online function, that will display data (all the online channels)
 			online(data);
-		} else if (data.stream === null ){
+		} else if (data.stream === null){
 			// if the data is null (streaming offline), call the offline function, that will display data  (all the offline channels)
 			getUserData(val);
-		}
-		console.log(data);
-
-	}); // end getJSON
+		} 
+		//console.log(data);
+	});
 });  // end $.each
 
 
@@ -50,12 +49,28 @@ function getUserData(val) {
 	$.getJSON(channelUrl, function(data) {
 		offline(data);
 	});
-}
 
+}
 
 
 // if streamers are offline i'll get the data and display it
 function offline(data) {
+
+	// if the streamer does not exsist, display his name. I'll comment out this section because i don't like the idea of displaying a channel that does not exsist, 
+	// it ruins the project in my opinion, if you would like to see if it works, just remove the comments and add "comster404" to the array of streamers
+
+
+	// START COMMENT OUT SECTION
+
+	// if(data.status === 404){
+	// 	var messages = data.message;
+	// 	var displayNotExsistingData = '<div class="col-xs-3 col-md-8" id="pm">' + messages + '</div>';
+	// 	$('.offline-display').append(messages);
+	// } else {
+
+
+
+
 	// getting data from the API
 	var status = data.status;
 	var name = data.display_name;
@@ -64,7 +79,6 @@ function offline(data) {
 	var game = "Offline";
 	var description = data.description;
 	var message = "This channel does not exsist";
-
 	// displaying data
 	var displayOfflineData = '<div class="row ' + 
           status + '"><div class="col-xs-2 col-md-1"><img src="' + 
@@ -72,16 +86,15 @@ function offline(data) {
           url + '" target="_blank">' + 
           name + '</a></div><div class="col-xs-3 col-md-8">'+ 
           game + '</div></div>';
-
-    
      	$('.offline-display').append(displayOfflineData);
+	//console.log(data);
 
 
 
-	console.log(data);
+// }  END COMMENT OUT SECTION
+
+
 }
-
-
 
 
 
